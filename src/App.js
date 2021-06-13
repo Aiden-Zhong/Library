a71265363a803cb547ec03f143dc73467177ed4f
Alvin Zhong <chinazhongyingjie@outlook.com>
2021-06-13 16:40:29 -0500

    REACT-COURSE

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from './components/Todo';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Explore React'/>
+    </div>
+  )
+}
+export default App
